6fc646b0943fb7a4833f44e60c63a16edda1978f
scotty brown <scottabrown89@gmail.com>
2017-12-05 23:27:33 +0000

    Adds appalachian_trail models

--- appalachian_trail/models.py
+++ appalachian_trail/models.py
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.db import models
 
-# Create your models here.
+class HikingDay(models.Model):
+    day = models.DateField()
+    mile = models.DecimalField(max_digits=8, decimal_places=1)
+    state = models.CharField(max_length=2)
+    location = models.CharField(max_length=255)
+    day_off = models.BooleanField()
+    precipitation = models.BooleanField()
+    precip_status = models.CharField(max_length=100)
+    notes = models.TextField()
