9969361eb9a480cb8084d1bc07a63c3338a5428e
scotty brown <scottabrown89@gmail.com>
2017-12-07 17:02:46 +0000

    Changes order by each day

--- appalachian_trail/views.py
+++ appalachian_trail/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from appalachian_trail.models import HikingDay
 
 def index(request):
-    hiking_days = HikingDay.objects.all()
+    hiking_days = HikingDay.objects.order_by('day')
     return render(request, 'appalachian_trail/index.html', {
         'hiking_days': hiking_days,
     })
