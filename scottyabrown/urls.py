c83cf23aa1e4345ac7cacf735988c5af1e8981f7
scotty brown <scottabrown89@gmail.com>
2018-02-05 21:51:11 +0000

    Adds appalachian trail views, templates, urls

--- scottyabrown/urls.py
+++ scottyabrown/urls.py
@@ -11,7 +11,12 @@ urlpatterns = [
     url(r'^callback/$', arock_views.callback, name='callback'),
     url(r'^add_songs/', arock_views.add_songs, name='arock_add_songs'),
     url(r'^athensrock/dashboard/', arock_views.dashboard, name='arock_dashboard'),
+    url(r'^appalachian_trail/show', at_views.show, name='at_show'),
     url(r'^appalachian_trail/index', at_views.index, name='at_index'),
+    url(r'^appalachian_trail/dashboard', at_views.dashboard, name='at_dashboard'),
+    url(r'^appalachian_trail/pictures', at_views.pictures, name='at_pictures'),
+    url(r'^appalachian_trail/faq', at_views.faq, name='at_faq'),
+    url(r'^appalachian_trail/gear', at_views.gear, name='at_gear'),
     url(r'^admin/', admin.site.urls),
 ]
 
