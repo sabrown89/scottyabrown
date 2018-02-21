9bc3dabdf6feded517dd5ac2b1ca15803015ec33
scotty brown <scottabrown89@gmail.com>
2018-02-21 22:53:23 +0000

    Adds basic Blog list and detail templates with get_absolute_url

--- blog/models.py
+++ blog/models.py
@@ -18,3 +18,15 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
     photo = models.ImageField(upload_to='post', null=True, blank=True)
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year,
+                                                 self.publish.strftime('%m'),
+                                                 self.publish.strftime('%d'),
+                                                 self.slug])
