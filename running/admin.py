a6841207b177cfd995505d52052a1fff6e77f36f
scott brown <scottabrown89@gmial.com>
2019-01-07 15:29:43 -0500

    Adds 'RunningDay' to django admin page

--- running/admin.py
+++ running/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import RunningDay
 
-# Register your models here.
+
+class RunningDayAdmin(admin.ModelAdmin):
+    list_display = ['day',
+                    'mileage',
+                    'location',
+                    'type_of_run',
+                    'notes'
+    ]
+
+
+admin.site.register(RunningDay, RunningDayAdmin)
